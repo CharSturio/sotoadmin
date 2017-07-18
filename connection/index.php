6fa45f5561984adf45eb6aaae02625e9bc50ac48
CARLOS ENRIQUE RAMIREZ GARCIA <Hayabusa@MacBook-Pro-de-CARLOS.local>
2017-07-18 10:17:50 -0500

    x

--- connection/index.php
+++ connection/index.php
@@ -1,5 +1,5 @@
 <?php
-$link = mysqli_connect('mysql.hostinger.mx', 'u324073230_sotoa', 'sotoadmin','u324073230_sotoa')
+$link = mysqli_connect('db625491732.db.1and1.com', 'dbo625491732', 'sotoadmin','db625491732')
   or die('No se pudo conectar: ' . mysqli_connect_error());
   // $link = mysqli_connect('localhost', 'root', 'admin','sotoadmin')
   //   or die('No se pudo conectar: ' . mysqli_connect_error());

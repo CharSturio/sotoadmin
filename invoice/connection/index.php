d897f0f0c7105eca69ae5711da1d43cb078ca4c5
Char <cerg.udeg@gmail.com>
2017-08-20 14:55:14 -0500

    iuabsd

--- invoice/connection/index.php
+++ invoice/connection/index.php
@@ -1,5 +1,5 @@
 <?php
-  $link = mysqli_connect('localhost', 'root', 'SotoLlantas2015','sotoadmin')
+  $link = mysqli_connect('localhost', 'admin', 'SotoLlantas2015','sotoadmin')
     or die('No se pudo conectar: ' . mysqli_connect_error());
  // mysql_select_db('sotoadmin', $link) or die ('No se pudo seleccionar la base de datos');
 ?>

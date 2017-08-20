8ba7112ef2609db7b46ad4fe73fa8b00fb733ece
Char <cerg.udeg@gmail.com>
2017-08-20 15:01:02 -0500

    changes

--- links/login/index.php
+++ links/login/index.php
@@ -6,7 +6,7 @@ require '../../connection/index.php';
 
   if ($_REQUEST['operation'] === 'login') {
     //$query = "SELECT * FROM users where user ='" . $name ."' AND password=SHA('" . $pass . "') LIMIT 1;";
-    $query = "SELECT * FROM users as U INNER JOIN permissions as P ON U.permit = P.name where U.user ='charly' AND U.password=SHA('" . $pass . "') LIMIT 1;";
+    $query = "SELECT * FROM users as U INNER JOIN permissions as P ON U.permit = P.name where U.user ='" . $name ."' AND U.password=SHA('" . $pass . "') LIMIT 1;";
     $result = mysqli_query($link, $query) or die ('Consulta fallida: ' . mysqli_error($link));
 
     if(mysqli_num_rows($result) === 1){

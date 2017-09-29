8d54d2b92c0eb740a21267554b0f1e564a41f1a8
Char <cerg.udeg@gmail.com>
2017-09-29 00:18:50 -0500

    ok

--- links/create/xml_credit.php
+++ links/create/xml_credit.php
@@ -141,7 +141,7 @@ require 'call.php';
     $node_dom_catch = $node_catch->appendChild($dom->createElement('cfdi:Domicilio'));
     $node_dom_catch->setAttribute("calle",$rowDandC['address']);
     $node_dom_catch->setAttribute("noExterior",$rowDandC['noExt']);
-    if ($rowDandC['noInt']) {
+    if (isset($rowDandC['noInt'])) {
       $node_dom_catch->setAttribute("noInterior",$rowDandC['noInt']);
     }
     $node_dom_catch->setAttribute("colonia",$rowDandC['colony']);

4e32b0ab07fe81955979187d6637b8f052b8f5bc
Char <cerg.udeg@gmail.com>
2017-09-30 17:33:01 -0500

    conection

--- connection/index.php
+++ connection/index.php
@@ -3,7 +3,8 @@
   //or die('No se pudo conectar: ' . mysqli_connect_error());
   // $link = mysqli_connect('localhost', 'admin', 'SotoLlantas2015.','sotoadmin')
   // or die('No se pudo conectar: ' . mysqli_connect_error());
-  $link = mysqli_connect('localhost', 'root', 'SS2013MySql2017.','sotoadmin')
+  //$link = mysqli_connect('localhost', 'root', 'SS2013MySql2017.','sotoadmin')
+  $link = mysqli_connect('localhost', 'root', 'SotoLlantas2015.,.','sotoadmin')
   or die('No se pudo conectar: ' . mysqli_connect_error());
  // mysql_select_db('sotoadmin', $link) or die ('No se pudo seleccionar la base de datos');
 ?>

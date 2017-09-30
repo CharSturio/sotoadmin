00bfb63f03adc3278eb581c47298e9e39369be3e
Char <cerg.udeg@gmail.com>
2017-09-30 18:39:28 -0500

    connection

--- invoice/connection/index.php
+++ invoice/connection/index.php
@@ -1,5 +1,6 @@
 <?php
   $link = mysqli_connect('localhost', 'admin', 'SotoLlantas2015','sotoadmin')
     or die('No se pudo conectar: ' . mysqli_connect_error());
+    $passSMTP = "SotoGoodyear2015.,";
  // mysql_select_db('sotoadmin', $link) or die ('No se pudo seleccionar la base de datos');
 ?>

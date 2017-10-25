7dc52f5d4a85d738d1fcc9bb98957ee227aecd9f
Charly <cramirez@marstom.com>
2017-10-25 10:33:04 -0500

    add campos

--- invoice/transfer.php
+++ invoice/transfer.php
@@ -28,23 +28,27 @@ $result = mysqli_query($link,$query) or die ('Consulta fallida: ' . mysqli_error
       <div class="clearL"></div>
       <br /><br />
       <div class="wid10"><b>Código</b></div>
-      <div class="wid15"><b>Clave Producto</b></div>
-      <div class="wid15"><b>Sucursal Salida</b></div>
-      <div class="wid15"><b>Sucural Entrada</b></div>
-      <div class="wid15"><b>Usuario</b></div>
-      <div class="wid15"><b>Cantidad</b></div>
-      <div class="wid15"><b>Fecha</b></div>
+      <div class="wid12"><b>Clave Producto</b></div>
+      <div class="wid12"><b>Nombre</b></div>
+      <div class="wid12"><b>Modelo</b></div>      
+      <div class="wid12"><b>Sucursal Salida</b></div>
+      <div class="wid12"><b>Sucural Entrada</b></div>
+      <div class="wid10"><b>Usuario</b></div>
+      <div class="wid10"><b>Cantidad</b></div>
+      <div class="wid10"><b>Fecha</b></div>
       <div class="clearL"></div>';
-      $query = "SELECT T.id,P.key_, B1.name AS Bout, B2.name AS Bin, U.name AS Nuser, T.amount, T.last_date FROM `translates` AS T INNER JOIN stocks AS S ON S.id = T.id_stock INNER JOIN products AS P ON P.id = S.id_product INNER JOIN branches AS B1 ON B1.id = T.id_branch_out INNER JOIN branches AS B2 ON B2.id = T.id_branch_in INNER JOIN users AS U ON U.id = T.id_user WHERE T.id_trans_op ='" . $id . "'";
+      $query = "SELECT T.id,P.key_, B1.name AS Bout, B2.name AS Bin, U.name AS Nuser, T.amount, T.last_date, P.name, P.model FROM `translates` AS T INNER JOIN stocks AS S ON S.id = T.id_stock INNER JOIN products AS P ON P.id = S.id_product INNER JOIN branches AS B1 ON B1.id = T.id_branch_out INNER JOIN branches AS B2 ON B2.id = T.id_branch_in INNER JOIN users AS U ON U.id = T.id_user WHERE T.id_trans_op ='" . $id . "'";
       $result = mysqli_query($link,$query) or die ('Consulta fallida: ' . mysqli_error($link));
         while($row = mysqli_fetch_assoc($result)){
           $html .= '<div class="wid10">' . $row['id'] . '</div>
-          <div class="wid15">' . $row['key_'] . '</div>
-          <div class="wid15">' . $row['Bout'] . '</div>
-          <div class="wid15">' . $row['Bin'] . '</div>
-          <div class="wid15">' . $row['Nuser'] . '</div>
-          <div class="wid15">' . $row['amount'] . '</div>
-          <div class="wid15">' . $row['last_date'] . '</div>
+          <div class="wid12">' . $row['key_'] . '</div>
+          <div class="wid12">' . $row['name'] . '</div>
+          <div class="wid12">' . $row['model'] . '</div>
+          <div class="wid12">' . $row['Bout'] . '</div>
+          <div class="wid12">' . $row['Bin'] . '</div>
+          <div class="wid10">' . $row['Nuser'] . '</div>
+          <div class="wid10">' . $row['amount'] . '</div>
+          <div class="wid10">' . $row['last_date'] . '</div>
           <div class="clearL"></div>';
         }
 
